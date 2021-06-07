fb275addc08957b4332ff010fe7b6d5fd11a15d6
Amit Kharade <amitkharade8200@gmail.com>
2021-06-07 20:55:57 +0400

    Added 4_1 MUX

--- 4_1 MUX/Mux4_1_tb.v
+++ 4_1 MUX/Mux4_1_tb.v
@@ -1,4 +1,6 @@
+// Module Name: Mux_tb
 
+//////////////////////////////////////////////////////////////////////////////////
 
 module Mux_tb;
 reg a,b,c,d;
@@ -7,7 +9,7 @@ wire y;
 Mux41 uut (a,b,c,d,s,y);
 
 initial begin
-a=1'b0; b=1'b0; c=1'b0; d=1'b0;
+	a=1'b0; b=1'b0; c=1'b0; d=1'b0;
 end
 
 always #5 a= ~a;
@@ -16,11 +18,11 @@ always #20 c= ~c;
 always #40 d= ~d;
 
 initial begin
-s = 2’b00;
-#80  s = 2’b01;
-#80  s = 2’b10;
-#80  s = 2’b11;
-#80  $stop;
+	s = 2’b00;
+	#80  s = 2’b01;
+	#80  s = 2’b10;
+	#80  s = 2’b11;
+	#80  $stop;
 end
     
 endmodule
--- Full Adder/HA.v
+++
@@ -1,8 +0,0 @@
-
-module HA(
-    input a,b,
-    output s, cout
-    );
-    assign s = a ^ b;
-    assign cout = a & b;
-endmodule
