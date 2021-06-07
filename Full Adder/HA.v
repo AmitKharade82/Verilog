7d9f7999a2ab085646704670f4ee59b15495dba6
Amit Kharade <amitkharade8200@gmail.com>
2021-06-07 20:57:44 +0400

    Added Full Adder

---
+++ Full Adder/HA.v
@@ -0,0 +1,11 @@
+// Module Name: HA
+
+//////////////////////////////////////////////////////////////////////////////////
+
+module HA(
+    input a,b,
+    output s, cout
+    );
+    assign s = a ^ b;
+    assign cout = a & b;
+endmodule
