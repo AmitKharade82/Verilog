d53e3265e7937f1891082533460ece1e96edfd6e
Amit Kharade <amitkharade8200@gmail.com>
2021-06-07 20:59:13 +0400

    Added Ripple Carry Adder

---
+++ Ripple Carry Adder/HA.v
@@ -0,0 +1,15 @@
+
+// Module Name: HA
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module HA(
+    input a,b,
+    output s, cout
+    );
+    assign s = a ^ b;
+    assign cout = a & b;
+endmodule
+
---
+++ Ripple Carry Adder/RCA.v
@@ -0,0 +1,16 @@
+ 
+// Module Name: RCA
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rca(
+    input [3:0] x,y,
+    input cin,
+    input [4:0] sum
+    );
+    wire c1, c2, c3;
+        FA fa1(.a(x[0]), .b(y[0]), .cin(cin), .s(sum[0]), .cout(c1));
+        FA fa2(.a(x[1]), .b(y[1]), .cin(c1), .s(sum[1]), .cout(c2));
+        FA fa3(.a(x[2]), .b(y[2]), .cin(c2), .s(sum[2]), .cout(c3));
+        FA fa4(.a(x[3]), .b(y[3]), .cin(c3), .s(sum[3]), .cout(sum[4]));
+endmodule
